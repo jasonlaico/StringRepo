5b3e2320f4ae00f307dd841b7980bd8a1a83ef04
185770 <185770@USTEXPLNGRR4QN2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-06-28 13:35:23 -0500

    firstcommit

---
+++ SimpleProject/src/com/demo/StringDemo.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String s1= "Hello";
+		String s2="World";
+		if (s1.contentEquals(s2))
+			System.out.println("String identical");
+		else 
+			System.out.println("Strings not identical");
+	}
+
+}
